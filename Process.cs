4d3d729e06be9906b41cce42e87c2459511f02c7
AnLoffredo <38223101+AnLoffredo@users.noreply.github.com>
2019-09-03 09:21:09 -0700

    Add files via upload

---
+++ Process.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Penguin.Console
+{
+    /// <summary>
+    /// Static class for managing console stuff
+    /// </summary>
+    public static class Process
+    {
+        /// <summary>
+        /// Runs an exe at the given path with the given args and returns the FINAL result to a string
+        /// </summary>
+        /// <param name="path">The path of the application to run</param>
+        /// <param name="args">The application arguments</param>
+        /// <returns>The full STD out as a string</returns>
+        public static string Run(string path, params string[] args)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+
+            startInfo.RedirectStandardInput = true;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.CreateNoWindow = true;
+            startInfo.UseShellExecute = false;
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = path;
+            startInfo.Arguments = string.Join(" ", args);
+            process.StartInfo = startInfo;
+            process.Start();
+
+            process.WaitForExit();
+
+            return process.StandardOutput.ReadToEnd();
+        }
+    }
+}
