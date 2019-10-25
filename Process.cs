c5018ca731024c7349b64deee3a05f00d29b457a
anloffredo <loffredo.ap@gmail.com>
2019-10-25 02:30:35 -0700

    Auto Commit

--- Process.cs
+++ Process.cs
@@ -1,6 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Diagnostics;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Penguin.Console
 {
@@ -10,29 +11,102 @@ namespace Penguin.Console
     public static class Process
     {
         /// <summary>
+        /// Starts a process with the given startinfo
+        /// </summary>
+        /// <param name="startInfo">the startinfo of the process</param>
+        /// <returns>the result of the run</returns>
+        public static ProcessResult Run(System.Diagnostics.ProcessStartInfo startInfo, int timeout = int.MaxValue)
+        {
+            StringBuilder output = new StringBuilder();
+            StringBuilder error = new StringBuilder();
+            ProcessResult Result = new ProcessResult();
+
+            using (System.Diagnostics.Process process = new System.Diagnostics.Process())
+            {
+                process.StartInfo = startInfo;
+                bool outputDone = false;
+                bool errorDone = false;
+                process.OutputDataReceived += (sender, e) => { if (e.Data is null) { outputDone = true; } else { output.Append(e.Data); } output.Append(System.Environment.NewLine); };
+                process.ErrorDataReceived += (sender, e) => { if (e.Data is null) { errorDone = true; } else { error.Append(e.Data); } error.Append(System.Environment.NewLine); };
+                process.Start();
+                process.BeginOutputReadLine();
+                process.BeginErrorReadLine();
+
+                //Task<string> stdOutTask = process.StandardOutput.ReadToEndAsync();
+                //Task<string> stdErrTask = process.StandardError.ReadToEndAsync();
+
+                bool ProcessDone()
+                {
+                    return process.HasExited || process.WaitForExit(5000);
+                }
+
+                bool StreamDone()
+                {
+                    return errorDone && outputDone;
+                }
+
+                while (!ProcessDone() || !StreamDone())
+                {
+                    if (ProcessDone())
+                    {
+                        //If the process is done and we havent gotten new stream information a whole second
+                        //later, we probably arent ever getting it
+                        System.Threading.Thread.Sleep(1000);
+
+                        break;
+                    }
+                }
+
+                Result.ExitCode = process.ExitCode;
+
+                process.Close();
+
+                Result.Output = output.ToString();
+                Result.Error = error.ToString();
+            }
+
+            return Result;
+        }
+
         /// Runs an exe at the given path with the given args and returns the FINAL result to a string
         /// </summary>
         /// <param name="path">The path of the application to run</param>
         /// <param name="args">The application arguments</param>
         /// <returns>The full STD out as a string</returns>
-        public static string Run(string path, params string[] args)
+        public static ProcessResult Run(string path, params string[] args)
         {
-            System.Diagnostics.Process process = new System.Diagnostics.Process();
-            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            return Run(GetStandardStartInfo(path, args));
+        }
 
+        /// <summary>
+        /// Runs an exe at the given path with the given args and returns the FINAL result to a string
+        /// </summary>
+        /// <param name="path">The path of the application to run</param>
+        /// <param name="workingDirectory">The working directory of the command</param>
+        /// <param name="args">The application arguments</param>
+        /// <returns>The full STD out as a string</returns>
+        public static ProcessResult RunIn(string path, string workingDirectory, params string[] args)
+        {
+            System.Diagnostics.ProcessStartInfo startInfo = GetStandardStartInfo(path, args);
+
+            startInfo.WorkingDirectory = workingDirectory;
+
+            return Run(startInfo);
+        }
+
+        internal static System.Diagnostics.ProcessStartInfo GetStandardStartInfo(string path, params string[] args)
+        {
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
             startInfo.RedirectStandardInput = true;
             startInfo.RedirectStandardOutput = true;
+            startInfo.RedirectStandardError = true;
+
             startInfo.CreateNoWindow = true;
             startInfo.UseShellExecute = false;
             startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
             startInfo.FileName = path;
             startInfo.Arguments = string.Join(" ", args);
-            process.StartInfo = startInfo;
-            process.Start();
-
-            process.WaitForExit();
-
-            return process.StandardOutput.ReadToEnd();
+            return startInfo;
         }
     }
 }
---
+++ ProcessResult.cs
@@ -0,0 +1,39 @@
+﻿namespace Penguin.Console
+{
+    /// <summary>
+    /// The result of a run process
+    /// </summary>
+    public class ProcessResult
+    {
+        /// <summary>
+        /// Results from the standard error stream
+        /// </summary>
+        public string Error { get; set; }
+
+        /// <summary>
+        /// The process exit code
+        /// </summary>
+        public int ExitCode { get; set; }
+
+        /// <summary>
+        /// Results from the standard output stream
+        /// </summary>
+        public string Output { get; set; }
+
+        /// <summary>
+        /// The output stream if exit code 0, otherwise the error stream
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            if (ExitCode == 0)
+            {
+                return Output;
+            }
+            else
+            {
+                return string.IsNullOrWhiteSpace(Error) ? Output : Error;
+            }
+        }
+    }
+}
